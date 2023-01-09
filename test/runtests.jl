d2ab675d7dc94d3b3bb9a5ae8c4954a63289d1b2
Benjamin Thomas Schwertfeger <development@b-schwertfeger.de>
2023-01-09 13:57:12 +0100

    added public endpoint tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,71 @@
-using KrakenEx
+using KrakenEx: SpotBaseRESTAPI, FuturesBaseRESTAPI
+import KrakenEx.SpotMarketModule as SpotMarket
+import KrakenEx.FuturesMarketModule as FuturesMarket
 using Test
 
-@testset "KrakenEx.jl" begin
-    # Write your tests here.
-    @test 1 == 1
+# Tests can only be performed on public endpoints, because
+# api keys are requred for private ones.
+
+@testset "KrakenEx Spot REST Market Endpoints" begin
+
+    client = SpotBaseRESTAPI()
+
+    @test typeof(SpotMarket.get_server_time(client)) == Int64
+
+    @test typeof(SpotMarket.get_assets(client)) == Dict{String,Any}
+
+    @test typeof(SpotMarket.get_tradable_asset_pair(client, pair=["XBTUSD"])) == Dict{String,Any}
+
+    @test typeof(SpotMarket.get_ticker(client, pair="XBTUSD")) == Dict{String,Any}
+
+    @test typeof(SpotMarket.get_ohlc(client, pair="XBTUSD")) == Dict{String,Any}
+
+    @test typeof(SpotMarket.get_order_book(client, pair="XBTUSD")) == Dict{String,Any}
+
+    @test typeof(SpotMarket.get_recend_spreads(client, pair="XBTUSD")) == Dict{String,Any}
+
+    @test typeof(SpotMarket.get_system_status(client)) == Dict{String,Any}
+
+    # ... 
+end
+
+@testset "KrakenEx Futures REST Market Endpoints" begin
+
+    client = FuturesBaseRESTAPI()
+
+    @test typeof(FuturesMarket.get_ohlc(client,
+        tick_type="trade",
+        symbol="PI_XBTUSD",
+        resolution="1m",
+        from=1668989233,
+        to=1668999233
+    )) == Dict{String,Any}
+
+    @test typeof(FuturesMarket.get_tick_types(client)) == Vector{Any}
+
+    @test typeof(FuturesMarket.get_tradeable_products(
+        client,
+        tick_type="mark"
+    )) == Vector{Any}
+
+    @test typeof(FuturesMarket.get_resolutions(
+        client,
+        tradeable="PI_XBTUSD",
+        tick_type="spot"
+    )) == Vector{Any}
+
+    @test typeof(FuturesMarket.get_fee_schedules(client)) == Dict{String,Any}
+
+    @test typeof(FuturesMarket.get_orderbook(client, symbol="PI_XBTUSD")) == Dict{String,Any}
+
+    @test typeof(FuturesMarket.get_tickers(client)) == Dict{String,Any}
+
+    @test typeof(FuturesMarket.get_instruments(client)) == Dict{String,Any}
+
+    @test typeof(FuturesMarket.get_instruments_status(client)) == Dict{String,Any}
+
+    @test typeof(FuturesMarket.get_trade_history(client, symbol="PI_XBTUSD")) == Dict{String,Any}
+
+    @test typeof(FuturesMarket.get_public_execution_events(client, tradeable="PI_XBTUSD")) == Dict{String,Any}
+    # ... 
 end
